4313e1132146b37204da57b446b2f8b57e8393b8
krittaphato3 <kritta.pun2549@gmail.com>
2025-11-22 00:35:10 +0700

    Optimization for GeneticSolver_5

--- src/algorithms/GeneticSolver.java
+++ src/algorithms/GeneticSolver.java
@@ -9,7 +9,6 @@ import java.util.stream.IntStream;
 
 public class GeneticSolver implements PathSolver {
 
-    // Ball is Gay, KEYES
     private int POPULATION_SIZE;
     private int MAX_GENERATIONS;
     private int GENOME_LENGTH;
@@ -17,28 +16,23 @@ public class GeneticSolver implements PathSolver {
     private static final double LOG_ONE_MINUS_RATE = Math.log(1.0 - MUTATION_RATE);
     private static final int TOURNAMENT_SIZE = 5;
 
-    private static final int[] dRow = {-1, 1, 0, 0};
-    private static final int[] dCol = {0, 0, -1, 1};
-
     @Override
     public String getName() {
-        return "GA (Skip-Mutation / Padding)";
+        return "GA (Pure 1D Arithmetic)";
     }
 
     @Override
     public List<Cell> solve(Maze maze) {
-        // Setup For KEYES
         int mapArea = maze.rows * maze.cols;
         POPULATION_SIZE = Math.min(3000, Math.max(1000, mapArea));
         GENOME_LENGTH = Math.min(8000, mapArea * 2);
         MAX_GENERATIONS = 1000;
+
         int paddedRows = maze.rows + 2;
         int paddedCols = maze.cols + 2;
         boolean[] wallMap = new boolean[paddedRows * paddedCols];
-
         Arrays.fill(wallMap, true);
-        
-        // Copy KEYES MAZE
+
         for (int r = 0; r < maze.rows; r++) {
             for (int c = 0; c < maze.cols; c++) {
                 if (!maze.grid[r][c].isWall) {
@@ -46,11 +40,11 @@ public class GeneticSolver implements PathSolver {
                 }
             }
         }
-        
-        int startR = maze.start.row + 1;
-        int startC = maze.start.col + 1;
-        int goalR = maze.goal.row + 1;
-        int goalC = maze.goal.col + 1;
+
+        int startIdx = (maze.start.row + 1) * paddedCols + (maze.start.col + 1);
+        int goalIdx = (maze.goal.row + 1) * paddedCols + (maze.goal.col + 1);
+
+        int[] moveOffsets = {-paddedCols, paddedCols, -1, 1};
 
         Individual[] population = new Individual[POPULATION_SIZE];
         Individual[] nextGen = new Individual[POPULATION_SIZE];
@@ -60,20 +54,19 @@ public class GeneticSolver implements PathSolver {
             nextGen[i] = new Individual(GENOME_LENGTH);
         }
 
-        initializePopulation(population, startR, startC, goalR, goalC);
-        
+        initializePopulation(population, startIdx, goalIdx, paddedCols);
+
         Individual globalBest = new Individual(GENOME_LENGTH);
         globalBest.fitness = -1;
 
-        // KEYES LOOP
         for (int gen = 0; gen < MAX_GENERATIONS; gen++) {
-            
+
             final Individual[] currentPop = population;
 
             IntStream.range(0, POPULATION_SIZE).parallel().forEach(i ->
-                evaluateFast(currentPop[i], wallMap, paddedCols, startR, startC, goalR, goalC)
+                evaluateFast(currentPop[i], wallMap, moveOffsets, startIdx, goalIdx, paddedCols)
             );
-            // Linear Scan for Rumen
+
             Individual genBest = currentPop[0];
             for (int i = 1; i < POPULATION_SIZE; i++) {
                 if (currentPop[i].fitness > genBest.fitness) {
@@ -90,16 +83,14 @@ public class GeneticSolver implements PathSolver {
 
             if (globalBest.reachedGoal) break;
 
-            // Parallel Breeding with Skip Mutation KEYESSSS Mak
             final Individual[] nextPopRef = nextGen;
-            
+
             System.arraycopy(globalBest.genes, 0, nextPopRef[0].genes, 0, GENOME_LENGTH);
-            
+
             IntStream.range(1, POPULATION_SIZE).parallel().forEach(i -> {
                 ThreadLocalRandom rand = ThreadLocalRandom.current();
                 Individual p1 = tournamentSelect(currentPop, rand);
                 Individual p2 = tournamentSelect(currentPop, rand);
-
                 produceChild(p1, p2, nextPopRef[i], rand);
             });
 
@@ -108,29 +99,22 @@ public class GeneticSolver implements PathSolver {
             nextGen = temp;
         }
 
-        return reconstructPath(globalBest, maze);
+        return reconstructPath(globalBest, maze, paddedCols);
     }
 
-    // This is KEYES Boundary Check and Evaluation
-    private void evaluateFast(Individual ind, boolean[] wallMap, int cols,
-        int startR, int startC, int goalR, int goalC) {
-        int currR = startR;
-        int currC = startC;
+    private void evaluateFast(Individual ind, boolean[] wallMap, int[] moveOffsets,
+                              int startIdx, int goalIdx, int width) {
+        int currIdx = startIdx;
         int steps = 0;
-        
         byte[] genes = ind.genes;
-        
-        for (int i = 0; i < GENOME_LENGTH; i++) {
-            byte move = genes[i];
-            int nextR = currR + dRow[move];
-            int nextC = currC + dCol[move];
 
-            //Check wall, to find Rumen
-            if (!wallMap[nextR * cols + nextC]) {
-                currR = nextR;
-                currC = nextC;
+        for (int i = 0; i < GENOME_LENGTH; i++) {
+            int nextIdx = currIdx + moveOffsets[genes[i]];
+            
+            if (!wallMap[nextIdx]) {
+                currIdx = nextIdx;
                 steps++;
-                if (currR == goalR && currC == goalC) {
+                if (currIdx == goalIdx) {
                     ind.reachedGoal = true;
                     ind.validStepsCount = steps;
                     ind.fitness = 1_000_000.0 - steps;
@@ -138,13 +122,19 @@ public class GeneticSolver implements PathSolver {
                 }
             }
         }
-        
+
         ind.reachedGoal = false;
         ind.validStepsCount = steps;
+        
+        int currR = currIdx / width;
+        int currC = currIdx % width;
+        int goalR = goalIdx / width;
+        int goalC = goalIdx % width;
+        
         int dist = Math.abs(currR - goalR) + Math.abs(currC - goalC);
         ind.fitness = 10_000.0 - (dist * dist);
     }
-    //Production_Pipeline KEYES
+
     private void produceChild(Individual p1, Individual p2, Individual child, ThreadLocalRandom rand) {
         int mid = rand.nextInt(GENOME_LENGTH);
         System.arraycopy(p1.genes, 0, child.genes, 0, mid);
@@ -152,11 +142,8 @@ public class GeneticSolver implements PathSolver {
 
         int index = 0;
         while (index < GENOME_LENGTH) {
-            
             double r = rand.nextDouble();
-            
             if (r == 0) r = 0.0000001;
-            
             int jump = (int) (Math.log(r) / LOG_ONE_MINUS_RATE);
             index += jump;
 
@@ -178,8 +165,14 @@ public class GeneticSolver implements PathSolver {
         return best;
     }
 
-    private void initializePopulation(Individual[] pop, int startR, int startC, int goalR, int goalC) {
+    private void initializePopulation(Individual[] pop, int startIdx, int goalIdx, int width) {
         ThreadLocalRandom rand = ThreadLocalRandom.current();
+        
+        int startR = startIdx / width;
+        int startC = startIdx % width;
+        int goalR = goalIdx / width;
+        int goalC = goalIdx % width;
+        
         int dR = goalR - startR;
         int dC = goalC - startC;
         int biasDir = -1;
@@ -198,16 +191,21 @@ public class GeneticSolver implements PathSolver {
         }
     }
 
-    private List<Cell> reconstructPath(Individual ind, Maze maze) {
+    private List<Cell> reconstructPath(Individual ind, Maze maze, int paddedWidth) {
         List<Cell> path = new ArrayList<>();
         int r = maze.start.row;
         int c = maze.start.col;
         path.add(maze.grid[r][c]);
 
+        int[] dRow = {-1, 1, 0, 0};
+        int[] dCol = {0, 0, -1, 1};
+
         for (int i = 0; i < ind.genes.length; i++) {
             if (path.size() > ind.validStepsCount + 1) break;
+            
             int nextR = r + dRow[ind.genes[i]];
             int nextC = c + dCol[ind.genes[i]];
+
             if (maze.isValid(nextR, nextC)) {
                 r = nextR;
                 c = nextC;
